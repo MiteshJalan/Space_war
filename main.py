328f1e803b8a9e9ed0db468c7a94b9077f4953bf
mjalan269@gmail.com <mjalan269@gmail.com>
2023-05-23 01:25:03 -0400

    Add enemy with boundry to player

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 #initialize pygame
 pygame.init()
 
@@ -17,8 +18,23 @@ playerY=480
 playerX_change=0
 playerY_change=0
 
+
+#Enemy
+enemyImg=pygame.image.load('ufo.png')
+enemyX=random.randint(0,800)
+enemyY=random.randint(50,150)
+enemyX_change=0
+enemyY_change=0
+
+
+
+
 def player(x,y):#sending coordiate of img
     screen.blit(playerImg, (x,y))#blit means draw on screen
+
+
+def enemy(x,y):#sending coordiate of img
+    screen.blit(enemyImg, (x,y))#blit means draw on screen
 
 
 #game loop
@@ -53,5 +69,15 @@ while running:
 
     playerX += playerX_change
     playerY+=playerY_change
+    if playerX<=0:
+        playerX=0
+    elif playerX>=736:
+        playerX=736
+    if playerY<=0:
+        playerY=0
+    elif playerY>=536:
+        playerY=536
+
     player(playerX,playerY) #call after fill to avaoid overlap screen above player
+    enemy(enemyX,enemyY)
     pygame.display.update()#to update screen every milisec as everything is moving
