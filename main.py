e7baf44efd259ec60649fc5d076e90317f1090ab
mjalan269@gmail.com <mjalan269@gmail.com>
2023-05-23 01:02:22 -0400

    keyboard input left right up down

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ pygame.display.set_icon(icon)
 playerImg=pygame.image.load('player.png')
 playerX=370
 playerY=480
+playerX_change=0
+playerY_change=0
 
 def player(x,y):#sending coordiate of img
     screen.blit(playerImg, (x,y))#blit means draw on screen
@@ -30,8 +32,26 @@ while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
-    
+        
+        
+        #press on key is event check if its left or right
+        if event.type==pygame.KEYDOWN:#IF KEY PRESSED
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.2
+            if event.key== pygame.K_RIGHT:
+                playerX_change = 0.2
+            if event.key== pygame.K_UP:
+                playerY_change= -0.2
+            if event.key== pygame.K_DOWN:
+                playerY_change= 0.2
+        if event.type==pygame.KEYUP:
+            if event.key== pygame.K_LEFT or event.key== pygame.K_RIGHT or event.key== pygame.K_UP or event.key== pygame.K_DOWN :
+                playerX_change = 0
+                playerY_change = 0
 
-    playerX+=0.1
+
+
+    playerX += playerX_change
+    playerY+=playerY_change
     player(playerX,playerY) #call after fill to avaoid overlap screen above player
     pygame.display.update()#to update screen every milisec as everything is moving
